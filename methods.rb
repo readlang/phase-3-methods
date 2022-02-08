a17ad7ba1986a9327e98f4b13071288759042f25
Read Langworthy <rlangworthy@gmail.com>
2022-02-07 16:15:01 -0800

    commit tree 365dd7dd7e0ebb50f09dd52f93ce82a489a696e3

--- methods.rb
+++ methods.rb
@@ -1,1 +1,64 @@
 # Your code here!
+
+
+
+
+def greet_programmer
+    puts("Hello, programmer!")
+end
+
+
+
+
+
+
+def greet (name)
+    puts("Hello, #{name}!")
+end
+
+
+
+
+
+
+
+
+
+
+def greet_with_default (name = "programmer")
+    puts("Hello, #{name}!")
+end
+
+
+
+
+
+
+
+def add (num1, num2)
+    return (num1 + num2)
+end
+
+
+
+
+
+
+
+def halve param
+    if param.class == Integer
+        return param/2
+    end
+    return nil
+end
+
+
+greet_programmer
+
+greet "brian"
+
+greet_with_default
+
+p add 2, 4
+
+p halve 4
